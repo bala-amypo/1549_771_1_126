dda115177168464db9857d427483107b72d8d0f8
bala-amypo <bala@amypo.in>
2025-12-16 14:41:31 +0000

    User commit

--- src/main/java/com/example/demo/model/PurchaseRecord.java
+++ src/main/java/com/example/demo/model/PurchaseRecord.java
@@ -13,6 +13,23 @@ public class PurchaseRecord{
  private Long customerId;  
   @Column(nullable=false)
   private Double amount;
+  @Column(nullable=false)
+  private LocalDate purchaseDate;
+  @Column(nullable=false)
+  private String storeLocation;
+
+  //Constructor
+  public PurchaseRecord(){
+
+  }
+  public PurchaseRecord(Long customerId,Double amount,LocalDate purchaseDate,String storeLocation){
+    this.customerId=customerId;
+    this.amount=amount;
+    this.purchaseDate=purchaseDate;
+    this.storeLocation=storeLocation;
+  }
+
+  //validation r
 
     
 
