d1d418d6a1fdd987fc43906293923787b24ee87a
bala-amypo <bala@amypo.in>
2025-12-16 13:31:30 +0000

    User commit

--- src/main/java/com/example/demo/model/CustomerProfile.java
+++ src/main/java/com/example/demo/model/CustomerProfile.java
@@ -10,12 +10,17 @@ public class CustomerProfile {
      private Long Id;//PK
      @Column(nullable=false,unique=true)
      private String customerId;
-     
+     @Column(nullable=false)
      private String fullname;
+     @Column(nullable=false,unique=true)
      private String email;
+     @Column(unique=true)
      private String phone;
+     @Column(nullable=false)
      private  String currentTier;
+     @Column(nullable=false)
      private Boolean active;
+     @Column (nullable=false)
      private LocalDateTime createdAt;
 
      
