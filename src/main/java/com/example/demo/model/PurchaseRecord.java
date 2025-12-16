1dafe261388ef61464c595450983f4a2a89db18c
bala-amypo <bala@amypo.in>
2025-12-16 14:16:31 +0000

    User commit

--- src/main/java/com/example/demo/model/PurchaseRecord.java
+++ src/main/java/com/example/demo/model/PurchaseRecord.java
@@ -1,6 +1,18 @@
 package com.example.demo.model;
 
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
 
+@Entity
+public class PurchaseRecord{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    
+
+    private CustomerProfile customer;
+    
+}
 
 public class PurchaseRecord{
     private long id;
