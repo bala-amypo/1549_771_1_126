524a384e5aa11221511748881db6dfc6927f21f7
bala-amypo <bala@amypo.in>
2025-12-16 16:16:44 +0000

    User commit

--- src/main/java/com/example/demo/model/TierHistoryRecord.java
+++ src/main/java/com/example/demo/model/TierHistoryRecord.java
@@ -1,1 +1,24 @@
 package com.example.demo.model;
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class TierHistoryRecord{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable=false)
+    private Long customerId;
+    @Column(nullable=false)
+    private String oldTier;
+    @Column(nullable=false)
+    private String newTier;
+    @Column(nullable=false)
+    private String reason;
+    @Column(nullable=false)
+    private LocalDateTime changedAt;
+
+    //constructors
+    
+
+}
