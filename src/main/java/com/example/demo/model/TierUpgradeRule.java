f071d2f24cbc85fcf57cc7184fca7da4d3200df0
bala-amypo <bala@amypo.in>
2025-12-16 15:46:31 +0000

    User commit

--- src/main/java/com/example/demo/model/TierUpgradeRule.java
+++ src/main/java/com/example/demo/model/TierUpgradeRule.java
@@ -6,5 +6,26 @@ public class TierUpgradeRule{
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
-    private String formTier
+    @Column(nullable=false)
+    private String formTier;
+    @Column(nullable=false)
+    private String toTier;
+    @Column(nullable=false)
+    private Double minSpend;
+    @Column(nullable=false)
+    private Integer minVisits;
+    @Column(nullable=false)
+    private Boolean active;
+
+    //Constructors
+
+    public TierUpgradeRule(){
+
+    }
+    public TierUpgradeRule(String formTier,String toTier,Double minSpend,Integer minVisits,Boolean active){
+        this.formTier=formTier;
+        this.toTier=toTier;
+        this.minSpend=minSpend;
+        this.minVisits=min
+    }
 }
