b53cf9989577f884d07037caae889dc8bef929fe
bala-amypo <bala@amypo.in>
2025-12-16 10:42:31 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/PurchaseRecord.java
@@ -0,0 +1,10 @@
+package com.example.demo.model;
+
+
+
+public class PurchaseRecord{
+    private long id;
+    private long customerId;
+    private double amount;
+    private LocalDate purchaseDate;
+}
