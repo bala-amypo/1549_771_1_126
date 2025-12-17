604ce521d581ebe2d5c473cfff488748317fa781
bala-amypo <bala@amypo.in>
2025-12-17 10:36:32 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/CustomerProfileServiceImpl.java
@@ -0,0 +1,16 @@
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    public CustomerServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    public CustomerProfile createCustomer(CustomerProfile customer) {
+        return customerRepository.save(customer);
+    }
+}
