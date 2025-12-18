1e9803a3914ec1ab9158961295e72f212480174d
bala-amypo <bala@amypo.in>
2025-12-18 04:24:07 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/CustomerProfileServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/CustomerProfileServiceImpl.java
@@ -1,16 +1,48 @@
+package com.example.demo5.service;
+
+import com.example.demo5.model.CustomerProfile;
+import com.example.demo5.repository.CustomerProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+
 @Service
-public class CustomerServiceImpl implements CustomerService {
+public class CustomerProfileServiceImpl implements CustomerProfileService {
 
-    private final CustomerRepository customerRepository;
-
-    public CustomerServiceImpl(CustomerRepository customerRepository) {
-        this.customerRepository = customerRepository;
-    }
+    @Autowired
+    private CustomerProfileRepository customerProfileRepository; 
 
     @Override
     public CustomerProfile createCustomer(CustomerProfile customer) {
-        return customerRepository.save(customer);
+        return customerProfileRepository.save(customer);
     }
+
+    // @Override
+    // public CustomerProfile getCustomerById(Long id) {
+    //     return customerProfileRepository.findById(id)
+    //             .orElseThrow(() -> new NoSuchElementException("Customer not found with id " + id));
+    // }
+
+    
+
+    @Override
+    public List<CustomerProfile> getAllCustomers() {
+        return customerProfileRepository.findAll();
+    }
+
+    // @Override
+    // public CustomerProfile updateTier(Long id, String newTier) {
+    //     CustomerProfile customer = getCustomerById(id);
+    //     customer.setCurrentTier(newTier);
+    //     return customerProfileRepository.save(customer);
+    // }
+
+    // @Override
+    // public CustomerProfile updateStatus(Long id, boolean active) {
+    //     CustomerProfile customer = getCustomerById(id);
+    //     customer.setActive(active);
+    //     return customerProfileRepository.save(customer);
+    // }
 }
