e98de8b315ae09c8a05753abcc4653e96b66dc96
bala-amypo <bala@amypo.in>
2025-12-18 03:54:07 +0000

    User commit

--- src/main/java/com/example/demo/model/CustomerProfile.java
+++ src/main/java/com/example/demo/model/CustomerProfile.java
@@ -1,89 +1,123 @@
-package com.example.demo.model;
+package com.example.demo5.model;
 
-import java.time.LocalDateTime;
 import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
+@Table(name = "customer_profiles")
 public class CustomerProfile {
+
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-     private Long Id;//PK
-     @Column(nullable=false,unique=true)
-     private String customerId;
-     @Column(nullable=false)
-     private String fullName;
-     @Column(nullable=false,unique=true)
-     private String email;
-     @Column(unique=true)
-     private String phone;
-     @Column(nullable=false)
-     private  String currentTier;
-     @Column(nullable=false)
-     private Boolean active;
-     @Column (nullable=false)
-     private LocalDateTime createdAt;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-public CustomerProfile(){
+    @Column(nullable = false, unique = true)
+    private String customerId;
 
-}
-public CustomerProfile(String customerId,String fullName,String email,String phone,String currentTier, Boolean active,LocalDateTime createdAt){
- this.customerId=customerId;
-this.fullName=fullName;
- this.email=email;
-this.phone=phone;
-this.currentTier=currentTier;
-this.active=active;
-this.createdAt=createdAt;
-}
-public void setId(Long id) {
-    Id = id;
-}
-public void setCustomerId(String customerId) {
-    this.customerId = customerId;
-}
-public void setFullName(String fullName) {
-    this.fullName = fullName;
-}
-public void setEmail(String email) {
-    this.email = email;
-}
-public void setPhone(String phone) {
-    this.phone = phone;
-}
-public void setCurrentTier(String currentTier) {
-    this.currentTier = currentTier;
-}
-public void setActive(Boolean active) {
-    this.active = active;
-}
-public void setCreatedAt(LocalDateTime createdAt) {
-    this.createdAt = createdAt;
-}
-public Long getId() {
-    return Id;
-}
-public String getCustomerId() {
-    return customerId;
-}
-public String getFullName() {
-    return fullName;
-}
-public String getEmail() {
-    return email;
-}
-public String getPhone() {
-    return phone;
-}
-public String getCurrentTier() {
-    return currentTier;
-}
-public Boolean getActive() {
-    return active;
-}
-public LocalDateTime getCreatedAt() {
-    return createdAt;
-}
+    @Column(nullable = false)
+    private String fullName;
 
+    @Column(nullable = false, unique = true)
+    private String email;
 
+    @Column(nullable = false, unique = true)
+    private String phone;
+
+    @Column(nullable = false)
+    private String currentTier;
+
+    @Column(nullable = false)
+    private Boolean active;
+
+    @Column(nullable = false, updatable = false)
+    @JsonIgnore
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.currentTier = "BRONZE";   // RULE APPLIED
+        this.active = true;
+        this.createdAt = LocalDateTime.now(); // RULE APPLIED
+    }
+
+    // getters & setters
+    // NO setter for createdAt
+public CustomerProfile(){}
+    public CustomerProfile(String customerId, String fullName, String email, String phone, String currentTier,
+            Boolean active) {
+        this.customerId = customerId;
+        this.fullName = fullName;
+        this.email = email;
+        this.phone = phone;
+        this.currentTier = currentTier;
+        this.active = active;
+    
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setCurrentTier(String currentTier) {
+        this.currentTier = currentTier;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getCurrentTier() {
+        return currentTier;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    
 }
-     
 
