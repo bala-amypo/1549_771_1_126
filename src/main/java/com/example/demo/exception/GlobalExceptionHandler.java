1aa17e23895ebfc1c791ba06e397b1ecb061c5d1
bala-amypo <bala@amypo.in>
2025-12-20 14:21:15 +0000

    User commit

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -1,27 +1,27 @@
-// package com.example.demo.exception;
+package com.example.demo.exception;
 
-// import org.springframework.http.HttpStatus;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.ExceptionHandler;
-// import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-// import java.time.LocalDateTime;
-// import java.util.HashMap;
-// import java.util.Map;
-// import java.util.NoSuchElementException;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
 
-// @RestControllerAdvice
-// public class GlobalExceptionHandler {
+@RestControllerAdvice
+public class GlobalExceptionHandler {
 
-//     @ExceptionHandler(NoSuchElementException.class)
-//     public ResponseEntity<Map<String, Object>> handleNoSuchElementException(
-//             NoSuchElementException ex) {
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<Map<String, Object>> handleNoSuchElementException(
+            NoSuchElementException ex) {
 
-//         Map<String, Object> response = new HashMap<>();
-//         response.put("timestamp", LocalDateTime.now());
-//         response.put("status", HttpStatus.NOT_FOUND.value());
-//         response.put("error", ex.getMessage());
+        Map<String, Object> response = new HashMap<>();
+        response.put("timestamp", LocalDateTime.now());
+        response.put("status", HttpStatus.NOT_FOUND.value());
+        response.put("error", ex.getMessage());
 
-//         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
-//     }
-// }
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+}
