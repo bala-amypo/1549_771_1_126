7e981648d7630ac69f4466049b4fe4aa15a82b3e
bala-amypo <bala@amypo.in>
2025-12-24 13:51:22 +0000

    User commit

--- src/main/java/com/example/demo/repository/TierUpgradeRuleRepository.java
+++ src/main/java/com/example/demo/repository/TierUpgradeRuleRepository.java
@@ -3,10 +3,10 @@ package com.example.demo.repository;
 import com.example.demo.model.TierUpgradeRule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+import java.util.Optional;
 
 public interface TierUpgradeRuleRepository
         extends JpaRepository<TierUpgradeRule, Long> {
 
-    List<TierUpgradeRule> findByActiveTrue();
+    Optional<TierUpgradeRule> findByFromTierAndToTier(String fromTier, String toTier);
 }
