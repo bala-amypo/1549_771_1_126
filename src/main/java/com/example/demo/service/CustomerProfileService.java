d28eeb02ea1e8ceec4bc7d774838d8dec74c29ef
bala-amypo <bala@amypo.in>
2025-12-24 13:03:22 +0000

    User commit

--- src/main/java/com/example/demo/service/CustomerProfileService.java
+++ src/main/java/com/example/demo/service/CustomerProfileService.java
@@ -1,10 +1,17 @@
+package com.example.demo.service;
+
+import com.example.demo.model.CustomerProfile;
+import java.util.List;
+
 public interface CustomerProfileService {
 
-    CustomerProfile create(CustomerProfile profile);
+    CustomerProfile create(CustomerProfile customer);
 
     CustomerProfile getById(Long id);
 
     List<CustomerProfile> getAll();
 
-    CustomerProfile updateStatus(Long id, boolean active);
+    CustomerProfile update(Long id, CustomerProfile customer);
+
+    void updateStatus(Long id, boolean active);
 }
